bb3ad53053d85dbd4557644372766ae52de067a5
junjie.zeng <junjie.zeng@outlook.com>
2021-05-12 09:17:32 +0800

    hooks

--- src/components/hooks-demo/Example6.js
+++ src/components/hooks-demo/Example6.js
@@ -2,20 +2,20 @@
 
 import { useEffect, useRef } from 'react'
 /*
-    useRef 保持标签对象，用于操作dom
+    useRef 保持标签对象，用于操作dom (useRef是一个容器，这个容器里面保存了ref所指定的节点)
 
 */
 
 function Example() {
-    const inputEl = useRef(null)
+    const inputEl = useRef()
 
     useEffect(()=>{
         console.log(inputEl)
     })
 
-    const onButtonClick = () => {
+    const getInputEl = () => {
         inputEl.current.value = 'hello word'
-        console.log('dom node',inputEl)
+        console.log('dom node',inputEl.current)
     }
 
 
@@ -23,7 +23,7 @@ function Example() {
         <div>
             <input ref={inputEl} type="text" /><br/><br/>
             <input ref={inputEl} type="text" /><br/><br/>
-            <button onClick={onButtonClick}>在input上展示文字</button>
+            <button onClick={getInputEl}>在input上展示文字</button>
         </div>
     )
 }
